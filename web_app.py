eb277d66f332e460d86015a89686c8779b4b6a3e
MaryyamFakhar39@gmail.com <144408177+Maryyam18@users.noreply.github.com>
2025-06-04 11:38:58 +0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 
-GEMINI_API_KEY = "AIzaSyDgGFIp97RuKkr8PijMkfHigFdDWSTVCIg"
+GEMINI_API_KEY = "rYour key"
 
 
 GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
